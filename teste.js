aeff7e9d21987b6b2a3c0ab8f95edbb67ccf7800
Maycon Porto <mayconporto063@gmail.com>
2024-02-03 12:55:35 -0300

    Operações aritiméticas

--- teste.js
+++ teste.js
@@ -9,3 +9,8 @@
 
 let num1 = 2;
 let num2 = 4;
+
+console.log("Soma: 2 + 4 =" + (num1 + num2));
+console.log("Subtração: 2 - 4 = " + (num1 - num2));
+console.log("Divisão: 2 / 4 = " + (num1 / num2));
+console.log("Multiplicação: 2 * 4 = " + (num1 * num2));
